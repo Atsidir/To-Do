523742141ac33ce5461cebbf93ca754f8997c6f7
Atsidir <tamas.torjai@gmail.com>
2016-10-10 15:10:13 +0200

    msodik

--- todo.py
+++ todo.py
@@ -1,10 +1,32 @@
 def lista():
 
     f=open('todo.txt','r')
+    line=f.readlines()
+    print(len(line))
 
-    for i in f:
-        print(i,end="")    
+array=[]
+
+
+def archive():
+    f=open('todo.txt','r')
+    
     f.close()
 
-lista()
+def beolvas():
+    f=open('todo.txt','r')
+    for i in f:
+        array.append(i)
+    f.close()
+
+def kiirat():
+    for j in range(len(array)):
+        print(j+1,". ",array[j],end="",sep="")
+    
+
+beolvas()
+archive()
+kiirat()
+
+
+
 
